a24d491ef8aac039e3e106ae468df56ca3d0c885
nstienen <n.l.stienen@student.tudelft.nl>
2024-05-14 11:35:18 +0200

    Tested all recorded datasets so far
    Good results on 13/19 datasets collected so far.
    Results that warranted a comment:
    1. Highly nonlinear dataset, reaching peak of 20rad in data
    2. Small (vertical) offset in angle for highest frequency signal recorded
    3. Slight underdamping for low frequency, low amplitude sinusoid
    4. Angle out of phase for no input, free fall signals
    5. Underdamped for low amplitude custom signal

--- validation.m
+++ validation.m
@@ -3,27 +3,48 @@ clc
 
 loadSim;
 
-% Choose which recorded data set to simulate
-% Note that only experiments denoted by Ef should be used here
-data = D06_05_h05_Ef_Ac08H0108_R01;
+% Choose which recorded data set to simulate by uncommenting
+% Note that experiments denoted with Ed have the angle locked
+% data = D05_08_h05_Ed_Asbc_R01;
+% data = D05_08_h05_Ef_Ab_03H01_R01;
+% data = D05_08_h05_Ef_Asbc_R01;
+% data = D05_13_h05_Ef_An_R01;
+% data = D05_13_h05_Ef_An_R02;
+data = D05_13_h05_Ef_Asbc_R01; % Underdamped
+% data = D06_05_h05_Ea_An00H00_R01; % Out of phase
+% data = D06_05_h05_Eb_An00H00_R01; % Out of phase
+% data = D06_05_h05_Ed_As10H05_R01;
+% data = D06_05_h05_Ef_Ab05H02_R01;
+% data = D06_05_h05_Ef_Ab05H05_R01;
+% data = D06_05_h05_Ef_Ac05H0108_R01;
+% data = D06_05_h05_Ef_Ac08H0108_R01;
+% data = D06_05_h05_Ef_As03H02_R01; % Slightly underdamped
+% data = D06_05_h05_Ef_As03H05_R01;
+% data = D06_05_h05_Ef_As03H10_R01; % Small offset
+% data = D06_05_h05_Ef_As05H05_R01;
+% data = D06_05_h05_Ef_As07H02_R01;
+% data = D06_05_h05_Ef_As10H05_R01; % Not even close (but to be expected)
 sampling_time = 0.05;
-initial_state = [0;0;0];
 
 time_vec  = data.data(:,1);
 val_angle = data.data(:,2);
 val_vel   = data.data(:,3);
+val_vel(1) = 0; % The first value is usually 300~500 while it should be 0.
 control   = data.data(:,5);
+initial_state = [val_angle(1);(val_angle(2)-val_angle(1))/sampling_time;0];
 
 theta = zeros(size(control));
 omega = zeros(size(control));
 
 xk = initial_state;
-for i = 1:size(control,1)
+theta(1) = xk(1);
+omega(1) = xk(3);
+for i = 2:size(control,1)
     [theta(i), omega(i), xk] = nlrk4(@system_equations, control(i), sampling_time, xk);
 end
 
 figure(1), clf;
-tiledlayout(2,1);
+tiledlayout(3,1);
 nexttile
 hold on;
 plot(time_vec, theta);
@@ -35,3 +56,8 @@ hold on;
 plot(time_vec, omega);
 plot(time_vec, val_vel);
 legend("simulation", "validation data")
+
+nexttile
+hold on
+plot(time_vec, control);
+legend("Control input")
