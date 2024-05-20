a16a5b6637e4ce9ea8557b2ae74b48f413d7bea9
nstienen <n.l.stienen@student.tudelft.nl>
2024-05-20 11:03:38 +0200

    Misc. updated files

--- flywheel_extended.m
+++ flywheel_extended.m
@@ -1,4 +1,4 @@
-function [dx,y] = flywheel_extended(t,x,u,g,M_f,l_f,M_b,l_b,J_R,J_bo,k_t,k_e,R,tau_fd,varargin)
+function [dx,y] = flywheel_extended(t,x,u,K1,K2,K3,K4,K5,varargin)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 y = [x(1);x(3)];
@@ -11,8 +11,8 @@ Omega_R = x(3);
 f1 = -1; % Defined as fix 1 to indicate where in code they are placed
 f2 = pi;
 
-Theta_ddot = (Omega_R*k_e*k_t - k_t*u + R*tau_fd*sign(Omega_R) + M_b*R*g*l_b*sin(f2 + Theta*f1) + M_f*R*g*l_f*sin(f2 + Theta*f1))/(R*f1*(J_bo + M_f*l_f^2));
-Omega_dot_R = -(J_R*R*tau_fd*sign(Omega_R) - J_bo*k_t*u - J_R*k_t*u + J_bo*R*tau_fd*sign(Omega_R) - M_f*k_t*l_f^2*u + J_R*Omega_R*k_e*k_t + J_bo*Omega_R*k_e*k_t + M_f*R*l_f^2*tau_fd*sign(Omega_R) + M_f*Omega_R*k_e*k_t*l_f^2 + J_R*M_b*R*g*l_b*sin(f2 + Theta*f1) + J_R*M_f*R*g*l_f*sin(f2 + Theta*f1))/(J_R*R*(J_bo + M_f*l_f^2));
+Theta_ddot = -K1*sin(Theta) -K2*Omega_R -K3*sign(Omega_R);
+Omega_dot_R = K4*sin(Theta) +K2*Omega_R +K3*sign(Omega_R) -K5*u;
 
 
 dx = [Theta_dot;
--- nlrk4.m
+++ nlrk4.m
@@ -1,12 +1,19 @@
 function [theta, omega, xk1] = nlrk4(nlf, control, h, xk)
 
-    % Executes one timestep of the RK4 algorithm
-    k1 = nlf(0, xk, control);
-    k2 = nlf(0 + h/2, xk + h/2*k1, control);
-    k3 = nlf(0 + h/2, xk + h/2*k2, control);
-    k4 = nlf(0 + h, xk + h*k3, control);
-    xk1 = xk + h/6*(k1 + 2*k2 + 2*k3 + k4);
+    % % Executes one timestep of the RK4 algorithm
+    % k1 = nlf(0, xk, control);
+    % k2 = nlf(0 + h/2, xk + h/2*k1, control);
+    % k3 = nlf(0 + h/2, xk + h/2*k2, control);
+    % k4 = nlf(0 + h, xk + h*k3, control);
+    % xk1 = xk + h/6*(k1 + 2*k2 + 2*k3 + k4);
+
+    control;
+    h;
+    xk;
+    [~,xk1] = ode45(@(t,x) nlf(t,x,control),[0 h],xk);
     
+    xk1 = xk1(end,:)';
+
     % Take out states of interest
     theta = xk1(1);
     omega = xk1(3);
