ede7406386bcea840c6b68714efb4882ead44558
nstienen <n.l.stienen@student.tudelft.nl>
2024-05-20 15:05:22 +0200

    Code documentation

--- LAB_code/LAB_functions/LQR_gain.m
+++ LAB_code/LAB_functions/LQR_gain.m
@@ -1,19 +1,30 @@
-w1 = 1;
-w2 = 1;
-w3 = 1/250;
-n1 = 10;
-n2 = 1;
-n3 = 1e-2;
+% Calculates the LQR gain for the system
 
+% true for rate of change penalty, false for no roc. pen.
+% Note that simulink file also needs changes for roc=false
+roc = false;
 
-Q = [w1*n1, 0, 0;
-     0, w2*n2, 0;
-     0, 0, w3*n3];
-R = 1e1;
-L_roc = 10;
+% Normalization factors
+wx1 = 1;
+wx2 = 1;
+wx3 = 1/250;
+wu1 = 1;
 
-roc = 0;
+% Penalization factors
+qx1 = 10;
+qx2 = 1;
+qx3 = 1e-2;
+qu1 = 1e1;
+ql1 = 10;
 
+% Q,R,L matrices
+Q = [wx1*qx1, 0, 0;
+     0, wx2*qx2, 0;
+     0, 0, wx3*qx3];
+R = wu1*qu1;
+L_roc = ql1;
+
+% Calculate LQR gain
 if roc
     [A,B,C,Q,R,M] = rate_change_pen(A,B,Q,R,L_roc);
     
@@ -25,34 +36,30 @@ else
     disp("Yes, I've calculated the LQR gain...")
     disp(K_LQR)
 end
-% dlqr(sysd.A,sysd.B,Q,R)
 
 
-
-function [A,B,C,Q,R,M] = rate_change_pen(A,B,Q,R,L)
-
-% Augment the state with the previous control action
-% This allows us to formulate it as a standard LQR problem with cross
-% terms.
-% For reference look at exercise set 2, problem 5
-% x is now 16 states being: [u v w phi theta psi p q r X_b Y_b Z_b Omega1 Omega2 Omega3 mu]
-n_x = size(A,1);
-n_u = size(B,2);
-
-% Calculate terminal cost using the discrete algebraic Ricatti equation
-P = idare(A,B,Q,R);
-
-% Extend the system matrices
-A = [A, zeros(n_x,n_u);
-     zeros(n_u,n_x), zeros(n_u,n_u)];
-B = [B; eye(n_u)];
-C = eye(n_x+n_u);
-
-% Extend the weighing matrices
-Q = [Q,zeros(n_x,n_u);
-     zeros(n_u,n_x),L];
-R = R+L;
-M = -[zeros(n_x,n_u);L];
-
-
+% Function taken from our MPC project
+function [A,B,C,Q,R,M] = rate_change_pen(A,B,Q,R,L)  
+    % Augment the state with the previous control action
+    % This allows us to formulate it as a standard LQR problem with cross
+    % terms.
+    % For reference look at exercise set 2, problem 5
+    % x is now 16 states being: [u v w phi theta psi p q r X_b Y_b Z_b Omega1 Omega2 Omega3 mu]
+    n_x = size(A,1);
+    n_u = size(B,2);
+    
+    % Calculate terminal cost using the discrete algebraic Ricatti equation
+    P = idare(A,B,Q,R);
+    
+    % Extend the system matrices
+    A = [A, zeros(n_x,n_u);
+         zeros(n_u,n_x), zeros(n_u,n_u)];
+    B = [B; eye(n_u)];
+    C = eye(n_x+n_u);
+    
+    % Extend the weighing matrices
+    Q = [Q,zeros(n_x,n_u);
+         zeros(n_u,n_x),L];
+    R = R+L;
+    M = -[zeros(n_x,n_u);L];
 end
---
+++ LAB_code/LAB_functions/generate_custom_signal.m
@@ -0,0 +1,40 @@
+function control_signal = generate_custom_signal(h, plot_bool)
+    % Generates a custom control input signal
+    % The entire signal is hardcoded and was used for data collection
+    % during the system identification process.
+
+    T_f = 60;
+    T = 0:h:T_f;
+    u = zeros(size(T));
+    
+    % Sine signal
+    A = 0.5;
+    freq = 0.5*2*pi;
+    T1 = 0:h:10;
+    u_sin = A*sin(freq*T1);
+    
+    % Square signal
+    A = 0.25;
+    freq = 0.1*2*pi;
+    T2 = 0:h:20;
+    u_sq = A*square(freq*T2)+A;
+    
+    % chirp
+    A = 0.5;
+    T3 = 0:h:10;
+    u_chirp = A*chirp(T3, 0.1, T3(end), 0.8, 'linear');
+    
+    % Combine signals
+    u(1:201) = u_sin;
+    u(401:601) = u_chirp;
+    u(801:1201) = u_sq;
+    
+    % Final signal
+    control_signal = [T',u'];
+    
+    % Plot signal in case of tweaking
+    if plot_bool
+        figure(1),clf;
+        plot(T,u)
+    end
+end
