a887ccd2631cc1bc22f507926ed5438cc89ca0c7
ivandoornen <K.I.vanDoornen@student.tudelft.nl>
2024-06-04 16:15:13 +0200

    Working MPC on HW2

--- LAB_code/LAB_functions/LQR_gain.m
+++ LAB_code/LAB_functions/LQR_gain.m
@@ -10,13 +10,13 @@ qx1 = 100;
 qx2 = 1e0;
 qx3 = 1e0;
 qu1 = 5e-4;
-L_LQR = 20;
+L_LQR = 20
 
 % Q,R,L matrices
 Q = [wx1*qx1, 0, 0;
      0, wx2*qx2, 0;
-     0, 0, wx3*qx3];
-R = wu1*qu1;
+     0, 0, wx3*qx3]
+R = wu1*qu1
 
 % Calculate LQR gain
 if model_continuous
