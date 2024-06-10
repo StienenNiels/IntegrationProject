c89c2931ca239b3e99b4c31b3f9449a4e241183a
Niels <n.l.stienen@student.tudelft.nl>
2024-06-10 11:34:57 +0200

    Update Data Collection function

--- LAB_code/LAB_functions/saveSim.m
+++ LAB_code/LAB_functions/saveSim.m
@@ -1,8 +1,19 @@
-function [] = saveSim(simout)
+function [] = saveSim(simout,simout_DC,simout_nowrap, h)
     name = input('Enter the name for the data: ', 's');
     folder = 'H:\Desktop\IntegrationProject\CollectedSimData';
     filepath = fullfile(folder, [name '.mat']);
-    data = [simout.time,simout.signals.values];
+    data.time = simout.time;
+    data.pendulum_angle = simout.signals.values(:,2);
+    data.pendulum_angle_estimate = simout.signals.values(:,1);
+    data.pendulum_velocity_estimate = simout.signals.values(:,3);
+    data.flywheel_velocity = simout.signals.values(:,5);
+    data.flywheel_estimate = simout.signals.values(:,4);
+    data.control = simout.signals.values(:,6);
+    data.control_unsaturated = simout.signals.values(:,7);
+    data.hybrid_state = simout.signals.values(:,8);
+    data.DC_current = simout_DC.signals.values;
+    data.pendulum_angle_unwrapped = simout_nowrap.signals.values(:,1);
+    data.sampling_time = h;
     save(filepath, 'data');
     disp('Data saved');
 end
